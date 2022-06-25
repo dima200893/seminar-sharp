661f741ecdb6064bb3509bcbce7e14a56fcc255d
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-25 22:14:59 +0300

    less3

---
+++ less3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("enter number: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("monday ");
+}
+    if (a == 2)
+    {
+        Console.WriteLine("vtornik");
+    }
+        if(a == 3)
+        {
+            Console.WriteLine("sreda ");
+        }
+            if(a == 4)
+            {
+                Console.WriteLine("chetverg");
+            }
+                if(a==5)
+                {
+                    Console.WriteLine(" pytnica");
+                }   
+                    if(a==6)
+                    {
+                        Console.WriteLine("subbota ");
+                    }
+                        if(a==7)    
+                        {
+                            Console.WriteLine("vosckresene ");
+                        }
+else if(a > 7)
+{
+    Console.WriteLine(" takix dney net ");
+}
