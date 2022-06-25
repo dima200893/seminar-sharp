90690955fc4b8a5add79ba8a3d18a4bb78864e3e
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-25 21:48:52 +0300

    less1

---
+++ less1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("enter number");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = a * a;
+
+Console.WriteLine("kvadrat "+ a + "=" + result);
+// конверттровать можно и по ждругому 
+
+// double b = (double) a / 3;
